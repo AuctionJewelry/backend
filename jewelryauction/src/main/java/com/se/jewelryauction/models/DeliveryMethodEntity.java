379883918c5fcdb889e6f348fb59120af50044ba
chinhhn173672 <ngocchinhpy23@gmail.com>
2024-06-02 11:56:26 +0700

    Update Entity 02/06/2024

--- jewelryauction/src/main/java/com/se/jewelryauction/models/DeliveryMethodEntity.java
+++ jewelryauction/src/main/java/com/se/jewelryauction/models/DeliveryMethodEntity.java
@@ -1,6 +1,6 @@
 package com.se.jewelryauction.models;
 
-import com.fasterxml.jackson.annotation.JsonBackReference;
+import com.se.jewelryauction.models.enums.DeliveryStatus;
 import jakarta.persistence.*;
 import lombok.*;
 
@@ -21,13 +21,12 @@ public class DeliveryMethodEntity extends BaseEntiy {
     @JoinColumn(name = "user_id")
     private UserEntity user;
 
+    @ManyToOne
+    @JoinColumn(name = "staff_id")
+    private UserEntity staff;
     private String full_name;
-
     private String phone_number;
-
     private String address;
-
-    private String tracking_number;
-
+    private DeliveryStatus status;
 
 }
--- jewelryauction/src/main/java/com/se/jewelryauction/models/enums/JewelryStatus.java
+++ jewelryauction/src/main/java/com/se/jewelryauction/models/enums/JewelryStatus.java
@@ -1,11 +1,26 @@
 package com.se.jewelryauction.models.enums;
 
 public enum JewelryStatus {
-    Valuating,
-    Pending,
-    Auctioning,
-    Auction_Success,
-    Auction_Fail,
-    Refund,
-    Refunded
+//    Valuating,
+//    Pending,
+//    Auctioning,
+//    Auction_Success,
+//    Auction_Fail,
+//    Refund,
+//    Refunded
+    PENDING,
+    ONLINE_VALUATED,
+    OFFLINE_VALUATING,
+    OFFLINE_VALUATED,
+    VALUATING_DELIVERING,
+    STORED,
+    AUCTIONING,
+    PRICE_CONFIRMING,
+    AUCTION_SUCCESS,
+    DELIVERING,
+    DELIVERED,
+    AUCTION_FAIL,
+    REFUNDING,
+    REJECT,
+    COMPLETED
 }
