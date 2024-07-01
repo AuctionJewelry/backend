80eeeffab0b68581b70ecd2bab7bbbc5edbd7057
tranthihongphuc <hongphuc0987@gmail.com>
2024-07-01 22:11:10 +0700

    brand update jewelry

--- jewelryauction/src/main/java/com/se/jewelryauction/repositories/IBrandRepository.java
+++ jewelryauction/src/main/java/com/se/jewelryauction/repositories/IBrandRepository.java
@@ -2,10 +2,12 @@ package com.se.jewelryauction.repositories;
 
 import com.se.jewelryauction.models.BrandEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface IBrandRepository extends JpaRepository<BrandEntity, Long> {
     boolean existsByName(String name);
-    BrandEntity findByName (String name);
-}
+    @Query("SELECT b FROM BrandEntity b WHERE LOWER(b.name) = LOWER(:name)")
+    BrandEntity findByName(@Param("name") String name);}
