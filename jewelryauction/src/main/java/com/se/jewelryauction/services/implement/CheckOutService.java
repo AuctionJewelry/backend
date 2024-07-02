a8ac65bc8642181ff92fb615c54219d0e4bbf998
Huỳnh Ngọc Chính <142372256+chinhhn173672@users.noreply.github.com>
2024-07-02 21:52:03 +0700

    CheckOut

--- jewelryauction/src/main/java/com/se/jewelryauction/services/implement/CheckOutService.java
+++ jewelryauction/src/main/java/com/se/jewelryauction/services/implement/CheckOutService.java
@@ -31,6 +31,63 @@ public class CheckOutService implements ICheckOutService {
 
     private final IUserRepository userRepository;
 
+    @Override
+    public DeliveryMethodEntity checkOutAuction(CheckOutRequest request) {
+        DeliveryMethodEntity deliveryMethod = new DeliveryMethodEntity();
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+        UserPrincipal userPrincipal = (UserPrincipal) authentication.getPrincipal();
+        UserEntity user = userPrincipal.getUser();
+        deliveryMethod.setUser(user);
 
+        AuctionEntity auction = auctionRepository.findById(request.getAuction_id())
+                .orElseThrow(() -> new AppException(HttpStatus.BAD_REQUEST, "Can not find the auction"));
+        if(!user.getId().equals(auction.getWinner().getId())){
+            throw new AppException(HttpStatus.BAD_REQUEST, "You do not have access");
+        }
+
+        if(auction.getStatus()!= AuctionStatus.Completed){
+            throw new AppException(HttpStatus.BAD_REQUEST, "Unable to pay");
+        }
+        JewelryEntity jewelry = auction.getJewelry();
+        deliveryMethod.setAddress(request.getAddress());
+        deliveryMethod.setFull_name(request.getFull_name());
+        deliveryMethod.setPhone_number(request.getPhone_number());
+        deliveryMethod.setJewelry(jewelry);
+        deliveryMethod.setStatus(DeliveryStatus.PREPARING);
+        deliveryMethod.setValuatingDelivery(true);
+        jewelry.setStatus(JewelryStatus.DELIVERING);
+        jewelry.setPrice(auction.getCurrentPrice());
+        jewelryRepository.save(jewelry);
+
+
+        return deliveryMethodRepository.save(deliveryMethod);
+    }
+
+    @Override
+    public DeliveryMethodEntity deliveringAuction(DeliveringRequest request){
+        DeliveryMethodEntity deliveryAuction = deliveryMethodRepository.findById(request.getDelivering_id())
+                .orElseThrow(() -> new AppException(HttpStatus.BAD_REQUEST, "Can not find the delivery auction"));
+        UserEntity staff = userRepository.findById(request.getStaff_id())
+                .orElseThrow(() -> new AppException(HttpStatus.BAD_REQUEST, "Can not find the staff"));
+        deliveryAuction.setStaff(staff);
+        deliveryAuction.setStatus(DeliveryStatus.DELIVERING);
+        return deliveryMethodRepository.save(deliveryAuction);
+    }
+    @Override
+    public DeliveryMethodEntity deliveredAuction(long id ){
+        DeliveryMethodEntity deliveryAuction = deliveryMethodRepository.findById(id)
+                .orElseThrow(() -> new AppException(HttpStatus.BAD_REQUEST, "Can not find the delivery auction"));
+        deliveryAuction.setReceiving_time(LocalDateTime.now());
+        deliveryAuction.setStatus(DeliveryStatus.DELIVERED);
+        return deliveryMethodRepository.save(deliveryAuction);
+    }
+    @Override
+    public DeliveryMethodEntity comfirmDelivery(long id){
+        DeliveryMethodEntity deliveryAuction = deliveryMethodRepository.findById(id)
+                .orElseThrow(() -> new AppException(HttpStatus.BAD_REQUEST, "Can not find the delivery auction"));
+        deliveryAuction.setStatus(DeliveryStatus.RECEIVED);
+        return deliveryMethodRepository.save(deliveryAuction);
+    }
 
 }
+
