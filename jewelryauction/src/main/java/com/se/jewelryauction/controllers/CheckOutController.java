0b374e7d3d977392084d306c67b5da18a9ee0c58
tranthihongphuc <hongphuc0987@gmail.com>
2024-07-02 21:47:50 +0700

    update

--- jewelryauction/src/main/java/com/se/jewelryauction/controllers/CheckOutController.java
+++ jewelryauction/src/main/java/com/se/jewelryauction/controllers/CheckOutController.java
@@ -13,28 +13,7 @@ import org.springframework.web.bind.annotation.*;
 @RequiredArgsConstructor
 public class CheckOutController {
     private final ICheckOutService checkOutService;
-    @PostMapping("")
-    public CoreApiResponse<DeliveryMethodEntity> checkOutAuction(@RequestBody CheckOutRequest request) {
-        DeliveryMethodEntity deliveryMethod;
-        deliveryMethod = checkOutService.checkOutAuction(request);
-        return  CoreApiResponse.success(deliveryMethod,"Check out successfully");
-    }
 
-    @PutMapping("/delivering")
-    public CoreApiResponse<DeliveryMethodEntity> deliveringAuction(@RequestBody DeliveringRequest request) {
-        DeliveryMethodEntity deliveryMethod = checkOutService.deliveringAuction(request);
-        return CoreApiResponse.success(deliveryMethod, "Delivering auction updated successfully");
-    }
 
-    @PutMapping("/delivered/{id}")
-    public CoreApiResponse<DeliveryMethodEntity> deliveredAuction(@PathVariable long id) {
-        DeliveryMethodEntity deliveryMethod = checkOutService.deliveredAuction(id);
-        return CoreApiResponse.success(deliveryMethod, "Delivered auction updated successfully");
-    }
 
-    @PutMapping("/confirm/{id}")
-    public CoreApiResponse<DeliveryMethodEntity> confirmDelivery(@PathVariable long id) {
-        DeliveryMethodEntity deliveryMethod = checkOutService.comfirmDelivery(id);
-        return CoreApiResponse.success(deliveryMethod, "Delivery confirmed successfully");
-    }
 }
