c290f5050a9a4eeed801cf37c11a68936e43e7b1
tranthihongphuc <hongphuc0987@gmail.com>
2024-07-16 16:33:08 +0700

    cancel auction

--- jewelryauction/src/main/java/com/se/jewelryauction/repositories/IValuatingRepository.java
+++ jewelryauction/src/main/java/com/se/jewelryauction/repositories/IValuatingRepository.java
@@ -1,12 +1,16 @@
 package com.se.jewelryauction.repositories;
 
+import com.se.jewelryauction.models.JewelryEntity;
 import com.se.jewelryauction.models.UserEntity;
 import com.se.jewelryauction.models.ValuatingEntity;
 import com.se.jewelryauction.models.enums.ValuatingStatus;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
+import java.util.Optional;
 
 @Repository
 public interface IValuatingRepository extends JpaRepository<ValuatingEntity, Long> {
@@ -14,5 +18,8 @@ public interface IValuatingRepository extends JpaRepository<ValuatingEntity, Lon
     List<ValuatingEntity> findByStaffId(Long staffId);
     List<ValuatingEntity> findByJewelrySellerId(UserEntity user);
     List<ValuatingEntity> findByStatusAndStaffId(ValuatingStatus status, Long staffId);
+
+    @Query("SELECT v FROM ValuatingEntity v WHERE v.jewelry = :jewelry AND v.isOnline = false")
+    Optional<ValuatingEntity> findByJewelryAndIsOnlineFalse(@Param("jewelry") JewelryEntity jewelry);
 
 }
--- jewelryauction/src/main/java/com/se/jewelryauction/scheduleds/AuctionSchedule.java
+++ jewelryauction/src/main/java/com/se/jewelryauction/scheduleds/AuctionSchedule.java
@@ -2,10 +2,12 @@ package com.se.jewelryauction.scheduleds;
 
 import com.se.jewelryauction.models.AuctionEntity;
 import com.se.jewelryauction.models.JewelryEntity;
+import com.se.jewelryauction.models.ValuatingEntity;
 import com.se.jewelryauction.models.enums.AuctionStatus;
 import com.se.jewelryauction.models.enums.JewelryStatus;
 import com.se.jewelryauction.repositories.IAuctionRepository;
 import com.se.jewelryauction.repositories.IJewelryRepository;
+import com.se.jewelryauction.repositories.IValuatingRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Service;
@@ -13,20 +15,23 @@ import org.springframework.stereotype.Service;
 import java.time.LocalDateTime;
 import java.time.ZoneId;
 import java.util.List;
+import java.util.Optional;
 
 @RequiredArgsConstructor
 @Service
 public class AuctionSchedule {
     private final IAuctionRepository auctionRepository;
     private final IJewelryRepository jewelryRepository;
+    private final IValuatingRepository valuatingRepository;
 
-    @Scheduled(fixedRate = 30000)
+    @Scheduled(fixedRate = 3000)
     public void updateStatusAuction() {
         List<AuctionEntity> auctions = auctionRepository.findAll();
         LocalDateTime now = LocalDateTime.now();
 
         for (AuctionEntity auction : auctions) {
             JewelryEntity jewelry = auction.getJewelry();
+            Optional<ValuatingEntity> valuatingEntityOptional = valuatingRepository.findByJewelryAndIsOnlineFalse(jewelry);
 
             LocalDateTime startTime = LocalDateTime.ofInstant(auction.getStartTime().toInstant(), ZoneId.systemDefault());
             LocalDateTime endTime = LocalDateTime.ofInstant(auction.getEndTime().toInstant(), ZoneId.systemDefault());
@@ -39,8 +44,16 @@ public class AuctionSchedule {
 
             if (auction.getStatus() == AuctionStatus.InProgress && endTime.isBefore(now)) {
                 if (auction.getWinner() != null) {
-                    auction.setStatus(AuctionStatus.Completed);
-                    jewelry.setStatus(JewelryStatus.AUCTION_SUCCESS);
+                    // Check if current price is less than valuating value
+                    valuatingEntityOptional.ifPresent(valuatingEntity -> {
+                        if (auction.getCurrentPrice() < valuatingEntity.getValuation_value()) {
+                            auction.setStatus(AuctionStatus.WaitingConfirm);
+                            jewelry.setStatus(JewelryStatus.AUCTION_SUCCESS);
+                        } else {
+                            auction.setStatus(AuctionStatus.Completed);
+                            jewelry.setStatus(JewelryStatus.AUCTION_SUCCESS);
+                        }
+                    });
                 } else {
                     auction.setStatus(AuctionStatus.Fail);
                     jewelry.setStatus(JewelryStatus.AUCTION_FAIL);
